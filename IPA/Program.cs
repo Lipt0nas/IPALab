4131062345927698ea4f482828c3c55506960f21
Martynas <gurmart@gmail.com>
2019-03-21 22:01:39 +0200

    Student class, input

--- IPA/Program.cs
+++ IPA/Program.cs
@@ -9,6 +9,34 @@ namespace IPA
     {
         static void Main(string[] args)
         {
+            List<Student> students = new List<Student>();
+
+            Console.WriteLine("Iveskite studenta:");
+
+            while (true)
+            {
+                Student s = new Student();
+                s.Create();
+
+                students.Add(s);
+
+                Console.WriteLine("Ivesti dar viena? (t - taip, n - ne)");
+                string choice = Console.ReadLine();
+
+                if(choice[0] == 'n')
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("{0, -15}{1, -15}{2, -15}", "Vardas", "Pavarde", "Galutinis(Vid.)");
+            Console.WriteLine("----------------------------------------------");
+            foreach(Student s in students)
+            {
+                Console.WriteLine("{0, -15}{1, -15}{2, -15}", s.name, s.lastName, s.getGradeAvg());
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ IPA/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPA
+{
+    class Student
+    {
+        public string name { set; get; }
+        public string lastName { set; get; }
+
+        public int examGrade { get; set; }
+
+        public List<int> homeworkGrades { get; }
+
+        public Student()
+        {
+            homeworkGrades = new List<int>();
+        }
+
+        public void AddHomeworkGrade(int grade)
+        {
+            homeworkGrades.Add(grade);
+        }
+
+        public float getGradeAvg()
+        {
+            int sum = homeworkGrades.Sum();
+
+            return 0.3f * ((float)sum / homeworkGrades.Count) + 0.7f * examGrade;
+        }
+
+        public void Create()
+        {
+            Console.Write("Iveskite studento varda: ");
+            name = Console.ReadLine();
+
+            Console.Write("Iveskite studento pavarde: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Iveskite studento egzamino pazymi: ");
+            examGrade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Iveskite studento namu darbu pazymius (-1 kad baigti): ");
+            int grade = 0;
+            while(true)
+            {
+                grade = int.Parse(Console.ReadLine());
+
+                if(grade == -1)
+                {
+                    break;
+                } else
+                {
+                    AddHomeworkGrade(grade);    
+                }
+            }
+        }
+    }
+}
