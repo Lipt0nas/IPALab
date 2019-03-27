96b63dbe4d225d8b2610ffc732f23c6c7f33df42
Martynas <gurmart@gmail.com>
2019-03-27 17:02:11 +0200

    Code cleanup

---
+++ IPA/Grader.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPA
+{
+    class Grader
+    {
+        private List<Student> students;
+
+        public Grader()
+        {
+            students = new List<Student>();
+        }
+
+        private void ShowMenu(string message, params Tuple<string, Action>[] choices)
+        {
+            Console.WriteLine(message);
+
+            for(int i = 0; i < choices.Length; i++)
+            {
+                Console.WriteLine("\t" + (i + 1) + ". " + choices[i].Item1);
+            }
+            
+            string rawInput = Console.ReadLine();
+            int input;
+            bool correctInput = int.TryParse(rawInput, out input);
+
+            if(correctInput)
+            {
+                input -= 1;
+
+                if(!(input >= 0 && input < choices.Length))
+                {
+                    correctInput = false;
+                }
+            }
+
+            if(correctInput)
+            {
+                choices[input].Item2();
+            } else
+            {
+                Console.Clear();
+                Console.WriteLine("Neteisingas pasirinkimas [" + rawInput + "] bandykite is naujo");
+                ShowMenu(message, choices);
+            }
+        }
+
+        public void InputStudents(bool random)
+        {
+            while (true) {
+                Console.Clear();
+                Student student = new Student();
+                
+                if(random)
+                {
+                    student.CreateRandom();
+                } else
+                {
+                    student.Create();
+                }
+
+                students.Add(student);
+
+                Console.Clear();
+
+                bool stop = false;
+                ShowMenu("Ar ivesti dar viena?",
+                    Tuple.Create<string, Action>("Taip", () => {}),
+                    Tuple.Create<string, Action>("Ne", () => { stop = true; })
+                );
+
+                if (stop) break;
+            }
+        }
+
+        private void ReadStudentsFromFile()
+        {
+
+        }
+
+        private void DisplayStudentGrades(bool median)
+        {
+            Console.WriteLine("{0, -15}{1, -15}{2, -15}", "Vardas", "Pavarde", "Galutinis(Vid.)");
+            Console.WriteLine("----------------------------------------------");
+
+            foreach (Student s in students)
+            {
+                Console.WriteLine("{0, -15}{1, -15}{2, -15}", s.Name, s.LastName, (median ? s.GetGradeMedian() : s.GetGradeAvg()).ToString("0.00"));
+            }
+        }
+
+        public void Run()
+        {
+            ShowMenu("Kaip sukurti studentus?",
+                Tuple.Create<string, Action>("Ivesti Ranka", () => {
+                    Console.Clear();
+                    ShowMenu("Ar generuoti studentu pazymius?", 
+                        Tuple.Create<string, Action>("Taip", () => { InputStudents(true); }),
+                        Tuple.Create<string, Action>("Ne", () => { InputStudents(false); })
+                    );
+                }),
+
+                Tuple.Create<string, Action>("Skaityti is failo", () => { ReadStudentsFromFile(); })
+            );
+
+            Console.Clear();
+
+            ShowMenu("Skaiciuoti",
+                Tuple.Create<string, Action>("Pazymiu vidurki", () => { DisplayStudentGrades(false); }),
+                Tuple.Create<string, Action>("Pazymiu mediana", () => { DisplayStudentGrades(true); })
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
--- IPA/Program.cs
+++ IPA/Program.cs
@@ -9,60 +9,8 @@ namespace IPA
     {
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>();
-
-            Console.WriteLine("Ar naudoti generuoti pazymius(1) ar ivesti ranka(2)?");
-            int rand = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Iveskite studenta:");
-
-            while (true)
-            {
-                Student s = new Student();
-
-                if (rand == 1)
-                {
-                    s.CreateRandom();
-                } else if(rand == 2)
-                {
-                    s.Create();                
-                }
-
-                students.Add(s);
-
-                Console.WriteLine("Ivesti dar viena? (t - taip, n - ne)");
-                string choice = Console.ReadLine();
-
-                if(choice[0] == 'n')
-                {
-                    break;
-                }
-            }
-
-
-            Console.WriteLine("Skaiciuoti vidurki(1) ar mediana(2)?");
-            int mode = int.Parse(Console.ReadLine());
-
-            if(mode == 1)
-            {
-                Console.WriteLine("{0, -15}{1, -15}{2, -15}", "Vardas", "Pavarde", "Galutinis(Vid.)");
-                Console.WriteLine("----------------------------------------------");
-                foreach (Student s in students)
-                {
-                    Console.WriteLine("{0, -15}{1, -15}{2, -15}", s.Name, s.LastName, s.GetGradeAvg().ToString("0.00"));
-                }
-            } 
-            else if(mode == 2)
-            {
-                Console.WriteLine("{0, -15}{1, -15}{2, -15}", "Vardas", "Pavarde", "Galutinis(Med.)");
-                Console.WriteLine("----------------------------------------------");
-                foreach (Student s in students)
-                {
-                    Console.WriteLine("{0, -15}{1, -15}{2, -15}", s.Name, s.LastName, s.GetGradeMedian().ToString("0.00"));
-                }
-            }
-
-            Console.ReadLine();
+            Grader grader = new Grader();
+            grader.Run();
         }
     }
 }
