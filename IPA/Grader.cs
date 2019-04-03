6959c27ab9b8bb34242ab2798a35a23c6b7370b1
Martynas <gurmart@gmail.com>
2019-04-03 18:01:48 +0300

    Cleaned up file parsing

--- IPA/Grader.cs
+++ IPA/Grader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace IPA
 {
@@ -76,54 +77,6 @@ namespace IPA
             }
         }
 
-        private void ReadStudentsFromFile()
-        {
-            Console.Clear();
-
-            string line;
-
-            try
-            {
-                System.IO.StreamReader file = new System.IO.StreamReader("kursiokai.txt");
-                while ((line = file.ReadLine()) != null)
-                {
-                    string[] args = line.Split(' ');
-
-                    if (args.Length != 8) continue;
-
-                    Student student = new Student();
-                    student.Name = args[0];
-                    student.LastName = args[1];
-                    for (int i = 0; i < 5; i++) {
-                        try
-                        {
-                            student.AddHomeworkGrade(int.Parse(args[2 + i]));
-                        } catch (Exception)
-                        {
-                            Console.WriteLine("Neisejo nuskaityti namu darbo pazymio [" + args[2 + i] + "]");
-                        }
-                    };
-
-                    try
-                    {
-                        student.ExamGrade = (int.Parse(args[7]));
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Neisejo nuskaityti egzamino pazymio [" + args[7] + "]");
-                    }
-
-                    students.Add(student);
-                }
-
-                file.Close();
-            } catch (Exception e)
-            {
-                Console.WriteLine("Ivyko klaida skaitant faila:");
-                Console.WriteLine(e.Message);
-            }
-        }
-
         private void DisplayStudentGrades(bool median)
         {
             Console.Clear();
@@ -167,7 +120,7 @@ namespace IPA
                     );
                 }),
 
-                Tuple.Create<string, Action>("Skaityti is failo", () => { ReadStudentsFromFile(); DisplayAllStudentGrades(); })
+                Tuple.Create<string, Action>("Skaityti is failo", () => { students = Reader.ReadStudentsFromFile<List<Student>>("kursiokai.txt"); DisplayAllStudentGrades(); })
             );
 
             Console.ReadKey();
